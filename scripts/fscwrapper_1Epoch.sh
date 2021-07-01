6fcfd1c451aebfda521a1304d381f4178c5c6e39
Pooneh Kalhori <pkalhori@n2198.(none)>
2021-07-01 07:41:22 -0700

    fsc scripts for rails

--- scripts/fscwrapper_1Epoch.sh
+++ scripts/fscwrapper_1Epoch.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 #$ -cwd
 #$ -l h_rt=20:00:00,h_data=1G
-#$ -N FscBirds
+#$ -N FscBirds3Epoch
 #$ -o /u/scratch/p/pkalhori/fastsimcoal/reports
 #$ -e /u/scratch/p/pkalhori/fastsimcoal/reports
 #$ -m abe
@@ -10,12 +10,12 @@
 
 muts="4.6e-9"
 samples="38"
-models="1D.2Epoch.LongT" 
+models="1D.3Epoch" 
 rundate=`date +%Y%m%d`
 #this is the string of populations to loop through
 # wd stands for "working directory"
 wd=/u/home/p/pkalhori/project-klohmueldata/pooneh_data/bird_inference
-md=$wd/modeldir
+gitdir=/u/home/p/pkalhori/project-klohmueldata/pooneh_data/github_repos/bird_inference
 fsc=/u/home/p/pkalhori/project-klohmueldata/pooneh_data/software/fsc26_linux64/fsc26
 for model in $models
 #iterate through each model
@@ -31,9 +31,9 @@ pop=All.$sample
 header=${model}_${pop}_${mut}_$rundate
 mkdir $header
 cd $header
-cp $md/$model.tpl ${model}_${pop}.tpl
-cp $md/$model.est ${model}_${pop}.est
-cp $wd/SFSdir/${pop}_MAFpop0.obs ${model}_${pop}_MAFpop0.obs
+cp $gitdir/modeldir/$model.tpl ${model}_${pop}.tpl
+cp $gitdir/modeldir/$model.est ${model}_${pop}.est
+cp $gitdir/SFSdir/${pop}_MAFpop0.obs ${model}_${pop}_MAFpop0.obs
 mkdir $wd/$header/run_${SGE_TASK_ID}
 cd $wd/$header/run_${SGE_TASK_ID}
 cp $wd/$header/${model}_${pop}.tpl $wd/$header/${model}_${pop}.est $wd/$header/${model}_${pop}_MAFpop0.obs ./
--- scripts/runnum.sh
+++ scripts/runnum.sh
@@ -1,24 +1,25 @@
-wd=/u/scratch/p/pkalhori/fastsimcoal/bird_data
-models="2D.2Epoch 2D.2Epoch.Mig"
-pops="RWBL_TRBL"
-#muts="4.6e-9"
-rundate=20191203
+#wd=/u/scratch/p/pkalhori/fastsimcoal/bird_data
+wd=/u/home/p/pkalhori/project-klohmueldata/pooneh_data/bird_inference
+models="1D.3Epoch"
+pops="All.38"
+muts="4.6e-9"
+rundate=20200226
 for model in $models
 do
 for pop in $pops
 do
-#for mut in $muts
-#do
-outfile=$wd/${model}_${pop}_${rundate}.all.output.concatted.txt
+for mut in $muts
+do
+outfile=$wd/${model}_${pop}_${mut}_${rundate}.all.output.concatted.txt
 #get header:
-header=`head -n1 $wd/${model}_${pop}_${rundate}/run_1/${model}_${pop}/*bestlhoods`
+header=`head -n1 $wd/${model}_${pop}_${mut}_${rundate}/run_1/${model}_${pop}/*bestlhoods`
 echo -e "runNum\t$header" > $outfile
 for i in {1..50}
 do 
-outdir=$wd/${model}_${pop}_${rundate}/run_${i}/${model}_${pop}
+outdir=$wd/${model}_${pop}_${mut}_${rundate}/run_${i}/${model}_${pop}
 results=`grep -v [A-Z] $outdir/*.bestlhoods`
 echo -e "${i}\t$results" >> $outfile
 done
 done
-#done
+done
 done
