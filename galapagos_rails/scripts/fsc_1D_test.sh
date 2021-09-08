568cde331d2df850a3529f11e2c80f384c0d25bf
Pooneh Kalhori <pkalhori@n2192.(none)>
2021-09-08 12:06:38 -0700

    making sure everything is updated

--- galapagos_rails/scripts/fsc_1D_test.sh
+++ galapagos_rails/scripts/fsc_1D_test.sh
@@ -1,25 +1,13 @@
-#! /bin/bash
-#$ -cwd
-#$ -l h_rt=20:00:00,h_data=1G
-#$ -N RailInference
-#$ -o /u/scratch/p/pkalhori/rails/fastsimcoal
-#$ -e /u/scratch/p/pkalhori/rails/fastsimcoal
-#$ -m abe
-#$ -M pkalhori
-#$ -t 1-50:1
-
-muts="4.6e-9"
+muts="1.4e-8 1.5e-8"
 samples="14"
 models="1D.2Epoch" 
-rundate=`date +%Y%m%d`
+rundate=test
 #this is the string of populations to loop through
 # wd stands for "working directory"
-wd=/u/home/p/pkalhori/project-klohmueldata/pooneh_data/github_repos/bird_inference/galapagos_rails
+wd=/u/home/p/pkalhori/project-klohmueldata/pooneh_data/bird_inference/galapagos_rails
 md=$wd/modeldir
 SFSdir=$wd/SFSdir
 fsc=/u/home/p/pkalhori/project-klohmueldata/pooneh_data/software/fsc26_linux64/fsc26
-outdir=/u/scratch/p/pkalhori/rails/inference/
-mkdir -p $outdir
 for model in $models
 #iterate through each model
 do
@@ -29,7 +17,7 @@ do
 for sample in $samples
 #iterate through different SFS sizes
 do
-cd $outdir
+cd $wd
 pop=PIN
 header=${model}_${pop}_${mut}_$rundate
 mkdir $header
@@ -37,13 +25,13 @@ cd $header
 cp $md/$model.tpl ${model}_${pop}.tpl
 cp $md/$model.est ${model}_${pop}.est
 cp $wd/SFSdir/${pop}_MAFpop0.obs ${model}_${pop}_MAFpop0.obs
-mkdir $outdir/$header/run_${SGE_TASK_ID}
-cd $outdir/$header/run_${SGE_TASK_ID}
-cp $outdir/$header/${model}_${pop}.tpl $outdir/$header/${model}_${pop}.est $outdir/$header/${model}_${pop}_MAFpop0.obs ./
+mkdir $wd/$header/run_3
+cd $wd/$header/run_3
+cp $wd/$header/${model}_${pop}.tpl $wd/$header/${model}_${pop}.est $wd/$header/${model}_${pop}_MAFpop0.obs ./
 #ss=`grep $pop $wd/projectionValues.txt|awk '{print$2}'`
 sed -i "s/MUT/$mut/g" ${model}_${pop}.tpl
 sed -i "s/SAMPLE_SIZE/$sample/g" ${model}_${pop}.tpl
-$fsc -t ${model}_${pop}.tpl -n100000 -m -e ${model}_${pop}.est -M -L 50 -q
+$fsc -t ${model}_${pop}.tpl -n1000 -m -e ${model}_${pop}.est -M -L 50 -q
 done
 done
 done
--- galapagos_rails/scripts/runnum.sh
+++ galapagos_rails/scripts/runnum.sh
@@ -1,24 +1,24 @@
-wd=/u/scratch/p/pkalhori/fastsimcoal/bird_data
-models="2D.2Epoch 2D.2Epoch.Mig"
-pops="RWBL_TRBL"
-#muts="4.6e-9"
-rundate=20191203
+wd=/u/scratch/p/pkalhori/rails/inference
+models="1D.1Epoch 1D.2Epoch"
+pops="PIN"
+muts="4.6e-9"
+rundate=20210703
 for model in $models
 do
 for pop in $pops
 do
-#for mut in $muts
-#do
-outfile=$wd/${model}_${pop}_${rundate}.all.output.concatted.txt
+for mut in $muts
+do
+outfile=$wd/${model}_${pop}_${mut}_${rundate}.all.output.concatted.txt
 #get header:
-header=`head -n1 $wd/${model}_${pop}_${rundate}/run_1/${model}_${pop}/*bestlhoods`
+header=`head -n1 $wd/${model}_${pop}_${mut}_${rundate}/run_1/${model}_${pop}/*bestlhoods`
 echo -e "runNum\t$header" > $outfile
 for i in {1..50}
 do 
-outdir=$wd/${model}_${pop}_${rundate}/run_${i}/${model}_${pop}
+outdir=$wd/${model}_${pop}_${mut}_${rundate}/run_${i}/${model}_${pop}
 results=`grep -v [A-Z] $outdir/*.bestlhoods`
 echo -e "${i}\t$results" >> $outfile
 done
 done
-#done
+done
 done
